9f3396c81a4b8ffcd50c7e4ed69ad762ce26fdd3
dronespear <s00130744@mail.itsligo.ie>
2015-12-07 12:14:46 +0000

    second change

--- AchievevementWebAPIExample/Controllers/ValuesController.cs
+++ AchievevementWebAPIExample/Controllers/ValuesController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 namespace AchievevementWebAPIExample.Controllers
 {
-    [Authorize]
+    
     public class ValuesController : ApiController
     {
         // GET api/values
